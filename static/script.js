e01b6af26c0cad5439be96c221e5c0eb33f31a44
JordanRoberts-2000 <jordanroberts654@outlook.com>
2021-09-16 23:16:11 +0000

    js testing still

--- static/script.js
+++ static/script.js
@@ -74,7 +74,8 @@ if ('scrollRestoration' in history) {
   });
   // new and improved/fixed loading
   $(window).on("load",function(){
-    $("#videoBackground").html('<source src="../navvid.mp4" type="video/mp4">')
+    console.log("here");
+    $("#videoBackground").html('<source src="{{url_for("navvid", filename="navvid.mp4")}}" type="video/mp4">')
     document.getElementById("videoBackground").addEventListener('loadeddata', (e) => {
       gridDisplay();
     });
