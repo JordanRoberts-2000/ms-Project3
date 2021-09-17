daa55555f652152b49e2ac7cd4877fbf50870d5a
JordanRoberts-2000 <jordanroberts654@outlook.com>
2021-09-17 05:04:12 +0000

    broke

--- app.py
+++ app.py
@@ -22,10 +22,13 @@ mongo = PyMongo(app)
 @app.route("/")
 def index():
     return render_template("index.html")
+
+    
 @app.route("/get_tasks")
 def get_tasks():
     tasks = mongo.db.tasks.find()
     return render_template("tasks.html", tasks=tasks)
+
 
 if __name__ == "__main__":
     app.run(
