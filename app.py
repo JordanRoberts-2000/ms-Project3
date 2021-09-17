4d62aad61fdac527c696d18665e885cd3e7f6234
JordanRoberts-2000 <jordanroberts654@outlook.com>
2021-09-17 06:01:31 +0000

    oowoo

--- app.py
+++ app.py
@@ -1,9 +1,5 @@
 import os
-from flask import (
-    Flask, flash, render_template,
-    redirect, request, session, url_for)
-from flask_pymongo import PyMongo
-from bson.objectid import ObjectId
+from flask import (Flask, render_template)
 if os.path.exists("env.py"):
     import env
 
@@ -11,18 +7,9 @@ if os.path.exists("env.py"):
 app = Flask(__name__)
 
 
-app.config["MONGO_DBNAME"] = os.environ.get("MONGO_DBNAME")
-app.config["MONGO_URI"] = os.environ.get("MONGO_URI")
-app.secret_key = os.environ.get("SECRET_KEY")
-
-
-mongo = PyMongo(app)
-
-
 @app.route("/")
 def index():
-    tasks = mongo.db.tasks.find()
-    return render_template("index.html", tasks=tasks)
+    return render_template("index.html")
 
 
 if __name__ == "__main__":
