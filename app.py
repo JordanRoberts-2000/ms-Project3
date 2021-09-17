e54bcef927aff92d02bc40617e85f629cb4440e0
JordanRoberts-2000 <jordanroberts654@outlook.com>
2021-09-17 06:24:22 +0000

    last hope

--- app.py
+++ app.py
@@ -10,23 +10,21 @@ if os.path.exists("env.py"):
 
 app = Flask(__name__)
 
-
 app.config["MONGO_DBNAME"] = os.environ.get("MONGO_DBNAME")
 app.config["MONGO_URI"] = os.environ.get("MONGO_URI")
 app.secret_key = os.environ.get("SECRET_KEY")
-
 
 mongo = PyMongo(app)
 
 
 @app.route("/")
-def index():
+@app.route("/get_tasks")
+def get_tasks():
     tasks = mongo.db.tasks.find()
-    return render_template("index.html" tasks=tasks)
+    return render_template("tasks.html", tasks=tasks)
 
 
 if __name__ == "__main__":
-    app.run(
-        host=os.environ.get("IP", "0.0.0.0"),
-        port=int(os.environ.get("PORT", "5000")),
-        debug=True)
+    app.run(host=os.environ.get("IP"),
+            port=int(os.environ.get("PORT")),
+            debug=True)
