14033c41838919a938b47e3a9d1969ab7934950f
JordanRoberts-2000 <jordanroberts654@outlook.com>
2021-09-20 10:27:12 +0000

    k

--- app.py
+++ app.py
@@ -18,14 +18,14 @@ app.secret_key = os.environ.get("SECRET_KEY")
 mongo = PyMongo(app)
 
 
-@app.route("/")
-@app.route("/get_tasks")
-def get_tasks():
-    tasks = mongo.db.tasks.find()
-    return render_template("index.html", tasks=tasks)
+#@app.route("/")
+#@app.route("/get_tasks")
+#def get_tasks():
+    #tasks = mongo.db.tasks.find()
+    #return render_template("tasks.html", tasks=tasks)
 
 
-@app.route("/", methods=["GET", "POST"])
+@app.route("/register", methods=["GET", "POST"])
 def register():
     if request.method == "POST":
         # check if username already exists in db
@@ -45,7 +45,7 @@ def register():
         # put the new user into 'session' cookie
         session["user"] = request.form.get("username").lower()
         flash("Registration Successful!")
-    
+    return render_template("index.html")
 
 
 if __name__ == "__main__":
